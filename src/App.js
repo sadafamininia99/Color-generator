afae05830cd91b0a273d9b1562bb79608fa8e596
sadafamininia99 <aminicomputer96@gmail.com>
2022-03-29 22:38:08 +0430

    get data from values

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import { useState } from "react";
+import react, { useState } from "react";
+import Values from "values.js";
 
 function App() {
   const [color, setColor] = useState("");
-  const [error, setError] = useState(false);
+  const [error, setError] = useState(false);    // errors or not 
+  const [list, setList] = useState(new Values("#FF7777").all(10));
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    try {
+      let colros = new Values(color).all(10)
+      setList(colros);
+    } catch (error) {
+      setError(true)
+      console.log(error);
+
+    }
+  };
 
   return (
     <>
@@ -11,12 +24,12 @@ function App() {
         <h2>
           Color Generator
         </h2>
-        <form type="text">
+        <form onSubmit={handleSubmit}>
           <input type="text" placeholder='Type color'
             onChange={(e) => setColor(e.target.value)}
-            className={`${error ? "error" : null}`}   
-            />
-            <button className="btn" typeof='submit' >Get Colors</button>
+            className={`${error ? "error" : null}`}
+          />
+          <button className="btn" typeof='submit' >Get Colors</button>
         </form>
       </section>
 
