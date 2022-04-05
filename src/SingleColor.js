6a3eef369da72183a4948335f1de90630e30cb3f
sadafamininia99 <aminicomputer96@gmail.com>
2022-04-05 21:09:34 +0430

    add single color to app.js

--- src/SingleColor.js
+++ src/SingleColor.js
@@ -1,17 +1,31 @@
-import React, { useState,useEffect } from 'react'
-import rgbToHex from './utils';
+import React, { useState, useEffect } from "react";
+import rgbToHex from "./utils";
 
-
-function SingleColor(rgb,weight,index,hexcolor) {
-    const [alert,setAlert]=useState(false);
-    const bcg=rgb.join(",")
-    const hex=rgbToHex(...rgb);
-    //spred operator 
+const SingleColor = ({ rgb, weight, index, hexColor }) => {
+  const [alert, setAlert] = useState(false);
+  const bcg = rgb.join(",");
+  const hex = rgbToHex(...rgb);
+  const hexValue = `#${hexColor}`;
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setAlert(false);
+    }, 3000);
+    return () => clearTimeout(timeout);
+  }, [alert]);
   return (
-    <div>
+    <article
+      className={`color ${index > 10 && "color-light"}`}
+      style={{ backgroundColor: `rgb(${bcg})` }}
+      onClick={() => {
+        setAlert(true);
+        navigator.clipboard.writeText(hexValue);
+      }}
+    >
+      <p className="percent-value">{weight}%</p>
+      <p className="color-value">{hexValue}</p>
+      {alert && <p className="alert">copied to clipboard</p>}
+    </article>
+  );
+};
 
-    </div>
-  )
-}
-
-export default SingleColor
+export default SingleColor;
