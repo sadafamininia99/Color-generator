b30149f2bc559d2c0cccc2eb159eb91974a025f8
sadafamininia99 <aminicomputer96@gmail.com>
2022-04-06 23:12:14 +0430

    commemnts

--- src/App.js
+++ src/App.js
@@ -1,40 +1,56 @@
-import react, { useState } from "react";
+import React, { useState } from "react";
 import Values from "values.js";
+import SingleColor from "./SingleColor";
 
 function App() {
   const [color, setColor] = useState("");
-  const [error, setError] = useState(false);    // errors or not 
+  const [error, setError] = useState(false);
   const [list, setList] = useState(new Values("#FF7777").all(10));
 
   const handleSubmit = (e) => {
     e.preventDefault();
     try {
-      let colros = new Values(color).all(10)
-      setList(colros);
+      let colors = new Values(color).all(10);
+      setList(colors);
     } catch (error) {
-      setError(true)
+      setError(true);
       console.log(error);
-
     }
   };
 
   return (
     <>
       <section className="container">
-        <h2>
-          Color Generator
-        </h2>
+        <h3>Color Generator</h3>
         <form onSubmit={handleSubmit}>
-          <input type="text" placeholder='Type color'
+          <input
+            type="text"
+            placeholder="#FF7777 or type new.."
+            value={color}
             onChange={(e) => setColor(e.target.value)}
             className={`${error ? "error" : null}`}
           />
-          <button className="btn" typeof='submit' >Get Colors</button>
+          <button className="btn" type="submit">
+            Get Colors
+          </button>
         </form>
       </section>
-
+      
+      <section className="colors">
+        {list.map((color, index) => {
+          return (
+            <SingleColor
+              key={index}
+              {...color}
+              index={index}
+              hexColor={color.hex}
+            />
+          );
+        })}
+      </section>
     </>
   );
 }
+//map around color
 
 export default App;
--- src/SingleColor.js
+++ src/SingleColor.js
@@ -5,6 +5,7 @@ const SingleColor = ({ rgb, weight, index, hexColor }) => {
   const [alert, setAlert] = useState(false);
   const bcg = rgb.join(",");
   const hex = rgbToHex(...rgb);
+  //clp functionality
   const hexValue = `#${hexColor}`;
   useEffect(() => {
     const timeout = setTimeout(() => {
